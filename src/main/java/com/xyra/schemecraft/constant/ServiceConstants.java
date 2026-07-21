5790df40ab516a868b728e2c5d51ff69ab09c21a
MarcoVisone <marcovisone5@gmail.com>
2026-07-21 16:18:58 +0200

    feat(Constant/ServiceConstants) Added MAX_PRODUCT_IMAGES

--- src/main/java/com/xyra/schemecraft/constant/ServiceConstants.java
+++ src/main/java/com/xyra/schemecraft/constant/ServiceConstants.java
@@ -4,4 +4,5 @@ public final class ServiceConstants {
     private ServiceConstants() {}
     public static final int ORDERS_PAGE_SIZE = 10;
     public static final int REVIEWS_PAGE_SIZE = 10;
+    public static final int MAX_PRODUCT_IMAGES = 10;
 }
